40de9eeb038047e08afdba38ce94e2354732bc77
IT24101219 <it24101219@my.sliit.lk>
2025-05-06 15:12:44 +0530

    Uploaded spring boot initializer file (Unzipped)
    Uploaded by IT24101219

---
+++ src/main/java/com/pgno49/salon_project/SalonProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pgno49.salon_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalonProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalonProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pgno49/salon_project/SalonProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgno49.salon_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
